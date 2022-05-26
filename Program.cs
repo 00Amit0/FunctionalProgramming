8465f8717989a9198fe56ef62d86f451f4d57dc2
“00Amit0” <amit836961@gmail.com>
2022-05-26 21:48:13 +0530

    Welcome to Basic Core Programs in C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace BasicCorePrograms
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to Basic Core Program");
+            //VowelOrConstant.CheckVowelOrConstant();
+            //SwapTwoNumbers.SwappingNumbers();
+            //Division.CalculateQuotientAndRemainder();
+            //EvenOrOdd.CheckEvenOrOdd();
+            //LeapYear.CheckLeapYear();
+            PowerOf2.CalculatePowerOf2();
+            FlipCoin.FlipTheCoin();
+            Factorization.Factor();
+            LargestNumber.FindingLargestNumber();
+            HarmonicNumber.Harmonic();
+        }
+    }
+}
