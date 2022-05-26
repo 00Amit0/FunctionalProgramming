3f780f2c9e5fed20e12df70ebfd3437e2f097bc1
“00Amit0” <amit836961@gmail.com>
2022-05-26 21:55:34 +0530

    Divide and know quotient and remainder

---
+++ Division.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Division
+    {
+        public static void CalculateQuotientAndRemainder()
+        {
+            int dividend = 50, divisor = 8;
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+            Console.WriteLine("Dividend : {0}, Diviosr : {1}", dividend, divisor);
+            Console.WriteLine("Quotient :" + quotient);
+            Console.WriteLine("Remainder :" + remainder);
+        }
+    }
+}
