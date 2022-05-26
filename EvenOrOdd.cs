447818062737ab1be3c19f2865635bf44e9e00ad
“00Amit0” <amit836961@gmail.com>
2022-05-26 21:57:24 +0530

    Find if the number is even or odd

---
+++ EvenOrOdd.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class EvenOrOdd
+    {
+        public static void CheckEvenOrOdd()
+        {
+            Console.WriteLine("Enter the number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Number is Even");
+            }
+            else
+            {
+                Console.WriteLine("Number is Odd");
+            }
+        }
+    }
+}
