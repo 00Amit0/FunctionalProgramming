faceff1bcd20e0d957324449712cdf6814aef6ab
“00Amit0” <amit836961@gmail.com>
2022-05-26 22:07:35 +0530

    Harmonic number looks fun

---
+++ HarmonicNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class HarmonicNumber
+    {
+        public static void Harmonic()
+        {
+            double HP = 0.0;
+            Console.WriteLine("Enter the Number :");
+            int N = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= N; i++)
+            {
+                Console.Write("1/{0}+", i);
+                HP += 1 / (float)i;
+            }
+            Console.Write("\n");
+            Console.WriteLine("Harmonic Number :" + HP);
+        }
+    }
+}
