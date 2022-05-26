74186f0523dd1d34abc126b5dc8fd52bf19b0a22
“00Amit0” <amit836961@gmail.com>
2022-05-26 22:01:14 +0530

    Prints values of exponents of 2

---
+++ PowerOf2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class PowerOf2
+    {
+        public static void CalculatePowerOf2()
+        {
+            Console.WriteLine("Enter the value to calculate power");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num <= 31)
+            {
+                for (int i = 0; i <= num; i++)
+                {
+                    Console.WriteLine(Math.Pow(2, i));
+                }
+            }
+            else
+            {
+                Console.WriteLine("Number not in rang between 0 to 31");
+            }
+        }
+    }
+}
