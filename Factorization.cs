361a9a93a77d137557b80de90261f1dd9059a4be
“00Amit0” <amit836961@gmail.com>
2022-05-26 22:04:57 +0530

    What are Factors ???

---
+++ Factorization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class Factorization
+    {
+        public static void Factor()
+        {
+            int N, x;
+            Console.WriteLine("Enter the Number ");
+            N = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The Factors are : ");
+            for (x = 1; x <= N; x++)
+            {
+                if (N % x == 0)
+                {
+                    Console.WriteLine(x);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
