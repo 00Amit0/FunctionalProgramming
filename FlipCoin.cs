17cf2441afa47596016a8ee42cfdc2b36c7f9cff
“00Amit0” <amit836961@gmail.com>
2022-05-26 22:02:55 +0530

    Enjoy flipping the coin

---
+++ FlipCoin.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCorePrograms
+{
+    internal class FlipCoin
+    {
+        public static void FlipTheCoin()
+        {
+            Console.WriteLine("Please enter the value for number of flips");
+            // int Head = 0;
+            // int Tail = 0;
+            int HeadCount = 0;
+            int TailCount = 0;
+            double HeadPercentage;
+            double TailPercentage;
+            int flipcoin = Convert.ToInt32(Console.ReadLine());
+            if (flipcoin > 0)
+            {
+                for (int i = 1; i <= flipcoin; i++)
+                {
+                    Random random = new Random();
+                    int number = random.Next(2);
+                    switch (number)
+                    {
+                        case 0:
+                            HeadCount++;
+                            break;
+                        case 1:
+                            TailCount++;
+                            break;
+                    }
+                    /*if (number == 1)
+                    {
+                        HeadCount++;
+                    }
+                    else
+                    {
+                        TailCount++;
+                    }*/
+                    Console.WriteLine(number);
+                }
+                Console.WriteLine("----------------");
+                HeadPercentage = HeadCount * 100 / flipcoin;
+                TailPercentage = TailCount * 100 / flipcoin;
+                Console.WriteLine("Head Percentage :" + HeadPercentage);
+                Console.WriteLine("Tail Percentage :" + TailPercentage);
+
+            }
+        }
+    }
+}
